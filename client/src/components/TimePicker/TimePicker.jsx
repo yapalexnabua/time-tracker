49cf5ea510deed3ac672875a9eb5beab14b36cb0
Alex <alexyap@Alexs-MacBook-Air.local>
2023-06-05 15:17:30 +0800

    hook up TimeTracker to Formik

--- client/src/components/TimePicker/TimePicker.jsx
+++ client/src/components/TimePicker/TimePicker.jsx
@@ -1,7 +1,17 @@
 import { PropTypes } from 'prop-types';
 import './TimePicker.css';
 
-const TimePicker = ({ className, onHoursChange, onSecondsChange }) => {
+const validateInput = (object) => {
+    if (object.target.value.length > object.target.maxLength) {
+        object.target.value = object.target.value.slice(0, object.target.maxLength)
+    }
+
+    if (parseInt(object.target.value) > object.target.max) {
+        object.target.value = object.target.max;
+    }
+}
+
+const TimePicker = ({ className, onHoursChange, onSecondsChange, valueHours, valueSeconds, disabled }) => {
     return (
         <div className={`grid grid-cols-5 items-center timepicker ${className}`}>
             <div className="col-span-2">
@@ -9,7 +19,10 @@ const TimePicker = ({ className, onHoursChange, onSecondsChange }) => {
                     step="1"
                     placeholder="00"
                     className="hours-input text-center w-full"
-                    onChange={onHoursChange} />
+                    onChange={(e) => onHoursChange(e.target.value)}
+                    disabled={disabled}
+                    value={valueHours}
+                    onFocus={(e) => e.target.select()} />
             </div>
 
             <span className="col-span-1 text-center">:</span>
@@ -19,7 +32,13 @@ const TimePicker = ({ className, onHoursChange, onSecondsChange }) => {
                     step="1"
                     placeholder="00"
                     className="seconds-input text-center w-full"
-                    onChange={onSecondsChange} />
+                    onChange={(e) => onSecondsChange(e.target.value)}
+                    disabled={disabled}
+                    value={valueSeconds}
+                    maxLength={2}
+                    max={59}
+                    onInput={validateInput}
+                    onFocus={(e) => e.target.select()} />
             </div>
         </div>
     );
@@ -28,11 +47,23 @@ const TimePicker = ({ className, onHoursChange, onSecondsChange }) => {
 TimePicker.propTypes = {
     onHoursChange: PropTypes.func.isRequired,
     onSecondsChange: PropTypes.func.isRequired,
-    className: PropTypes.string
+    className: PropTypes.string,
+    valueHours: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]),
+    valueSeconds: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]),
+    disabled: PropTypes.bool
 };
 
 TimePicker.defaultProps = {
-    className: ''
+    className: '',
+    disabled: false,
+    valueHours: '00',
+    valueSeconds: '00',
 };
 
 export default TimePicker;
--- client/src/components/TimeTracker/TimeTracker.jsx
+++ client/src/components/TimeTracker/TimeTracker.jsx
@@ -3,11 +3,24 @@ import { getProjects } from "../../api/projectsApi";
 import Select from 'react-select';
 import TimePicker from '../TimePicker/TimePicker';
 import './TimeTracker.css';
+import { useFormik } from "formik";
 
 const TimeTracker = () => {
     const { data: projects = [], isLoading } = useQuery('projects', getProjects, {
         retry: false,
         refetchOnWindowFocus: false
+    });
+    
+    const formik = useFormik({
+        initialValues: {
+            taskDescription: '',
+            project: null,
+            hours: '00',
+            seconds: '00'
+        },
+        onSubmit: (values) => {
+            console.log(values);
+        }
     });
 
     if (isLoading) {
@@ -18,22 +31,34 @@ const TimeTracker = () => {
 
     return (
         <div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <div className="grid grid-cols-7 items-center">
                     <div className="col-span-3 border border-black h-full">
                         <input type="text" 
                             placeholder="My awesome task"
-                            className="w-full h-full px-2" />
+                            className="w-full h-full px-2"
+                            name="taskDescription"
+                            value={formik.values.taskDescription}
+                            onChange={formik.handleChange} />
                     </div>
+
                     <Select options={
-                        projects.map(project => ({
-                            value: project._id,
-                            label: project.name
-                        }))
-                    } 
+                            projects.map(project => ({
+                                value: project._id,
+                                label: project.name
+                            }))
+                        } 
                         className="col-span-2 react-select-container"
-                        placeholder="Select a project..." />
-                    <TimePicker className="col-span-1 h-full" />
+                        placeholder="Select a project..."
+                        value={formik.values.project}
+                        onChange={(selected) => formik.setFieldValue('project', selected)} />
+
+                    <TimePicker className="col-span-1 h-full"
+                       valueHours={formik.values.hours}
+                       valueSeconds={formik.values.seconds}
+                       onHoursChange={(value) => formik.setFieldValue('hours', value)}
+                       onSecondsChange={(value) => formik.setFieldValue('seconds', value)} />
+                    
                     <button className="col-span-1 border border-black h-full w-full">Submit</button>
                 </div>
             </form>
