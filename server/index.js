12d702b2c4950ae378b2a9b1677eb9533e6b7abf
Alex <alexyap@Alexs-MacBook-Air.local>
2023-06-05 13:53:19 +0800

    add cors

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import timeLogRoutes from './routes/timeLogRoutes.js';
 import projectRoutes from './routes/projectRoutes.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/time-logs', timeLogRoutes);
